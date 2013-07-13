36b81fa42e1f2f2b80c7d819e2654ae4e3a250fa
vinamrata.singal <girlrockingguna@gmail.com>
2013-07-12 20:53:28 -0700

    initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Socialgiving::Application.config.secret_key_base = 'b8b415f5c97016a8cacf320052f05612cc85f4457199339f275c64e51ed4c899e216ed2f058622805ea77113bc14a79a1a25b57f7a9aed17c6b45b5e57ab0499'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Socialgiving::Application.config.session_store :cookie_store, key: '_socialgiving_session'
